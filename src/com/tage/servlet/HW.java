b7facc8272a9f6379abaa56f42b132ca679a5d69
tage <763409282@qq.com>
2015-11-14 02:07:38 +0800

    @WebServlet classses

--- src/com/tage/servlet/HW.java
+++ src/com/tage/servlet/HW.java
@@ -1,19 +1,23 @@
+package com.tage.servlet;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
- * Created by tage on 11/13/15.
+ * Created by tage on 11/14/15.
  */
-public class Hello extends HttpServlet {
+@WebServlet(urlPatterns = "/HW")
+public class HW extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().println("Hello World");
     }
 }
